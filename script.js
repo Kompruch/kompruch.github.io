34f3ff1d8fe5c8d3d9e7aea0a0395d1a4be3564c
Kompruch <iceblazz@hotmail.com>
2020-05-18 19:54:48 +0700

    commit5

--- script.js
+++ script.js
@@ -2,8 +2,19 @@
 let i = 0;
 document.querySelector("button").addEventListener("click", function () {
     let text = prompt('something'); 
-    document.querySelector('ul').appendChild(document.createElement('li'))
-    document.querySelectorAll('li')[i].innerHTML = text;
-    i ++;
+    
+    while (text == "") {
+        alert('add text')
+        text = prompt('somthing');
+    }
+
+    enlist(text);
+
+    function enlist (text) {
+        document.querySelector('ul').appendChild(document.createElement('li'))
+        document.querySelectorAll('li')[i].innerHTML = text;
+        i ++;
+    }
+
     });
     
