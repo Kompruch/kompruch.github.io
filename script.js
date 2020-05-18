f2351fda9163263267034afe650fdda4ba1d465c
Kompruch <iceblazz@hotmail.com>
2020-05-18 18:17:43 +0700

    commit 4

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
+
+let i = 0;
 document.querySelector("button").addEventListener("click", function () {
     let text = prompt('something'); 
-    let i = 0
-    i ++
     document.querySelector('ul').appendChild(document.createElement('li'))
     document.querySelectorAll('li')[i].innerHTML = text;
+    i ++;
     });
     
